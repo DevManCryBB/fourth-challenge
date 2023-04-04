1e2aa16a58f767d9b9416f870f2008d452930c8b
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-03 22:46:47 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,45 @@
-//References to DOM elements
+var timerEl = document.querySelector("#countdown-timer")
+var highScore = document.querySelector("#high-scores")
+var question1 = document.querySelector("#question1")
 
-var TimeLeft = document.getElementById
+
+// show a new container with the question and 4 choices
+question1 = document.getElementById("question1")
+
+
+
+// // check answers
+// // checkAnswer(event){
+    
+// // }
+
+// var TimeLeft = document.getElementById
+
+// Timer that counts down from 5
+function countdown() {
+    var timeLeft = 100;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+
+        timerEl.textContent = timeLeft + ' seconds remaining';
+    
+        timeLeft--;
+      } else if (timeLeft === 1) {
+    
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+    
+        timerEl.textContent = '';
+
+        clearInterval(timeInterval);
+
+        displayMessage();
+      }
+    }, 1000);
+  }
+
+  countdown()
+// }
